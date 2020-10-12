5483dcb64a3e5887db181561ec29db3143e3c44a
Bartek-Szewczyk <ba20001000@gmail.com>
2020-10-12 18:33:25 +0200

    create2.0

--- Szyfr-Vigenere/obj/Debug/MainWindow.g.cs
+++ Szyfr-Vigenere/obj/Debug/MainWindow.g.cs
@@ -52,7 +52,7 @@ namespace Szyfr_Vigenere {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Szyfr_Vigenere;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Szyfr-Vigenere;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
