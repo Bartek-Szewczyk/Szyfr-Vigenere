d4a9888deb6f450bdfafe143002fe10bd392bde2
Bartek-Szewczyk <ba20001000@gmail.com>
2020-10-14 12:35:01 +0200

    version1.0

--- Szyfr-Vigenere/MainWindow.xaml.cs
+++ Szyfr-Vigenere/MainWindow.xaml.cs
@@ -24,5 +24,156 @@ namespace Szyfr_Vigenere
         {
             InitializeComponent();
         }
+
+        static char[] letter = new char[] { 'a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'ń', 'o', 'ó', 'p', 'q', 'r', 's', 'ś', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ź', 'ż' };
+        static string output;
+        static char[] input;
+        static int res;
+
+
+        public static int getKey(string key)
+        {
+            int r = 0;
+            input = key.ToLower().ToCharArray();
+
+            for (int j = 0; j < input.Length; j++)
+            {
+                for (int i = 0; i < letter.Length; i++)
+                {
+                    if (input[j] == letter[i])
+                    {
+                        r = i;
+                    }
+                }
+            }
+
+            return r;
+        }
+
+        public static string Encryption(string plain_text, int key)
+        {
+            input = plain_text.ToLower().ToCharArray();
+
+            for (int j = 0; j < input.Length; j++)
+            {
+                for (int i = 0; i < letter.Length; i++)
+                {
+                    if (input[j] == letter[i])
+                    {
+                        input[j] = letter[(i + key) % letter.Length];
+                        break;
+                    }
+                }
+            }
+            output= new string(input);
+            return output;
+        }
+
+        public static string Decryption(string cipher_text, int key)
+        {
+            input = cipher_text.ToLower().ToCharArray();
+
+            for (int j = 0; j < input.Length; j++)
+            {
+                for (int i = 0; i < letter.Length; i++)
+                {
+                    if (input[j] == letter[i])
+                    {
+                        res = (i - key + letter.Length);
+                        input[j] = letter[res % letter.Length];
+                        break;
+                    }
+                }
+            }
+            output = new string(input);
+            return output;
+        }
+
+        string m;
+        string k;
+
+
+        public void wykonaj_Click(object sender, RoutedEventArgs e)
+        {
+            if (cmbBox.Text=="Szyfrowanie")
+            {
+                m = txt1.Text.Replace(" ", "");
+                k = txtkey.Text;
+                int r = 0;
+                string s =null;
+                for (int i = 0; i < m.Length; i++)
+                {
+                    if (r==k.Length-1)
+                    {
+                        r = 0;
+                    }
+                    else if (r<k.Length && i>0)
+                    {
+                        r++;
+                    }
+                    else if (r<k.Length && i == 0)
+                    {
+                        r = 0;
+                    }
+
+                    if (r<k.Length)
+                    {
+                        s+= Encryption(m.Substring(i,1).ToLower(), getKey(k.Substring(r,1).ToLower()));
+                    }
+                }
+
+                txt2.Text = s;
+
+            }
+            else
+            {
+                m = txt1.Text.Replace(" ", "");
+                k = txtkey.Text;
+                int r = 0;
+                string s = null;
+                for (int i = 0; i < m.Length; i++)
+                {
+                    if (r == k.Length - 1)
+                    {
+                        r = 0;
+                    }
+                    else if (r < k.Length && i > 0)
+                    {
+                        r++;
+                    }
+                    else if (r < k.Length && i == 0)
+                    {
+                        r = 0;
+                    }
+
+                    if (r < k.Length)
+                    {
+                        s += Decryption(m.Substring(i, 1).ToLower(), getKey(k.Substring(r, 1).ToLower()));
+                    }
+                }
+
+                txt2.Text = s;
+                
+            }
+
+        }
+
+        private void Clean()
+        {
+
+            txt1.Text = "Wprowadz Tekst";
+            txt2.Text = "";
+            txtkey.Text = "";
+            Encrypted.IsSelected = true;
+        }
+
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+            Clean();
+        }
     }
+
+
+
+
 }
